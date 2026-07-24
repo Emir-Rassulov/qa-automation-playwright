a0c0ffe88edc00765d3dd1f8015fb907eb1fdb05
Emir Rassulov <emir.rassulov@gmail.com>
2026-07-24 15:01:34 +0200

    example of parametrization

---
+++ tests/parametrized-test.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect, request } from '@playwright/test';
+
+const validationTestCases = [
+  { email: 'joe.doe@example.com', isValid: true },
+  { email: 'emir.rassulov@gmail.com', isValid: true },
+  { email: 'not-an-email', isValid: false },
+];
+
+for (const testCase of validationTestCases) {
+  test(`создание пользователя с email "${testCase.email}"`, async ({ request }) => {
+    const response = await request.post('https://jsonplaceholder.typicode.com/users', {
+      data: {
+        email: testCase.email,
+      },
+    });
+
+    expect(response.status()).toBe(201);
+  });
+}
